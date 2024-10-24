2824f47978049e8acc52384c07bb12de6f902c77
jackyjlxing <xingjl@hotmail.com>
2024-10-24 11:27:56 -0400

    llama3.2 for generation.

--- graph/chains/generation.py
+++ graph/chains/generation.py
@@ -1,8 +1,10 @@
 from langchain import hub
 from langchain_core.output_parsers import StrOutputParser
-from langchain_openai import ChatOpenAI
+# from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
 
-llm = ChatOpenAI(temperature=0)
+# llm = ChatOpenAI(temperature=0)
+llm = ChatOllama(temperature=0, model="llama3.2")
 prompt = hub.pull("rlm/rag-prompt")
 
 generation_chain = prompt | llm | StrOutputParser()
